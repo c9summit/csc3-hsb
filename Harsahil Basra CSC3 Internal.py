803d5f6657e78acc35f101b6ed0080e08c0057ac
c9summit <st21204@ormiston.school.nz>
2023-07-24 09:53:25 +1200

    testrun

---
+++ Harsahil Basra CSC3 Internal.py
@@ -0,0 +1,12 @@
+#Harsahil Basra CSC Internal
+#Sports program or basketball
+from tkinter import*
+
+root = Tk()
+
+#creating a label widget
+myLabel = Label(root, text="hello world")
+#shoving it onto the screen
+myLabel.pack()
+
+root.mainloop()
