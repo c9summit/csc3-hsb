2901f4d4f8168b37259fa22fe894036fd6df71ab
c9summit <st21204@ormiston.school.nz>
2023-08-14 10:03:05 +1200

    Adding frames to program thought they are currently empty so when run not all frames will be accessible

--- Harsahil Basra CSC3 Internal.py
+++ Harsahil Basra CSC3 Internal.py
@@ -1,13 +1,46 @@
 #Harsahil Basra CSC Internal
 #Sports program or basketball
 from tkinter import*
+from PIL import IMageTk, Image
 
-root = Tk()
+window = Tk()
+window.rowconfigure(0, weight=1)
+window.columnconfigure(0, weight=1)
+window.geometry("700x900")
 
-#creating a label widget
-myLabel = Label(root, text="hello world")
-#shoving it onto the screen
-myLabel.pack()
+#Creating frames
+page0 = Frame(window) #Entry Page
+page1 = Frame(window) #Login+signup
+page1a = Frame(window) #Login
+page1b = Frame(window) #Signup
+page2 = Frame(window) #Dashboard
+page3 = Frame(window) #Beginner
+page4 = Frame(window) #Advanced
+page5 = Frame(window) #Feedback
+page6 = Frame(window) #Profile
 
-root.mainloop()
-#test
+for frame in (page0, page1, page1a, page1b, page2, page3, page4, page5, page6):
+    frame.grid(row=0, column=0, sticky='nsew')
+
+def show_frame(frame):
+    frame.tkraise()
+
+show_frame(page0)
+
+# ========= Page 0 =========
+
+# ========= Page 1 =========
+
+# ========= Page 1a =========
+
+# ========= Page 1b =========
+
+# ========= Page 2 =========
+
+# ========= Page 3 =========
+
+# ========= Page 4 =========
+
+# ========= Page 5 =========
+
+# ========= Page 6 =========
